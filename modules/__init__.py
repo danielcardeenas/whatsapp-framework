69b9a36b9eee18334970c1844a25a84a5863f5c4
iLQ <danielcardeenas@gmail.com>
2017-07-14 19:08:22 +0000

    Cleaning example modules

--- modules/__init__.py
+++ modules/__init__.py
@@ -4,7 +4,5 @@ from modules.yesno import yesno
 from modules.wolfram import wolfram_mac
 from modules.hihelp import hihelp
 from modules.poll import poll
-#from modules.poll2.poll2 import PollKing
 #from modules.youtube.mac_youtube import WAYoutube
 #from modules.elo import elo, match
-#from modules.trip import trip
--- modules/trip/db/schema/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE debters (
-  id INTEGER PRIMARY KEY,
-  name TEXT NOT NULL,
-  airbnb REAL,
-  vuelo REAL
-);
