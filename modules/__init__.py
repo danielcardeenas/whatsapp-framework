22e221cae8feca4569f831c96b61cf1b52172c31
iLQ <danielcardeenas@gmail.com>
2017-07-14 19:35:41 +0000

    Module elo now using singlas

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,9 +1,7 @@
-# Add here the modules you want to enable
+# Add here the modules you want to enable here
 
 from modules.yesno import yesno
 from modules.wolfram import wolfram_mac
 from modules.hihelp import hihelp
 from modules.poll import poll
 from modules. elo import elo
-#from modules.youtube.mac_youtube import WAYoutube
-#from modules.elo import elo, match
