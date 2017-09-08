f4c34b8fe5ad09a2be1bfabbb4f68f3f9aa7a685
Daniel Cardenas <danielcardeenas@gmail.com>
2017-09-08 19:41:21 +0200

    Update hihelp.py

--- modules/hihelp/hihelp.py
+++ modules/hihelp/hihelp.py
@@ -5,7 +5,7 @@ Signals this module listents to:
 1. When a message is received (signals.message_received)
 ==========================================================
 '''
-@signals.command_received.connect
+@signals.message_received.connect
 def handle(message):
     if message.command == "hi":
         hi(message)
