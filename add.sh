b9e7b5cf6de57bc642713730f9b4de0281ffa9c4
iLQ <danielcardeenas@gmail.com>
2017-10-09 11:14:03 +0000

    Added direct message to phone number

--- add.sh
+++ add.sh
@@ -6,11 +6,4 @@ git add -A
 git reset config.py
 git reset modules/wolfram/config.py
 
-# Modules
-git rm --cached -r modules/broadcast
-git rm --cached -r modules/wolfram
-git rm --cached -r modules/yesno
-git rm --cached -r modules/poker
-git rm --cached -r modules/poll2
-
 git commit -m "$1"
