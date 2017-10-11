a6e61b6c9f3671cac03c2aea1f1a8eb2ebddd5c1
iLQ <danielcardeenas@gmail.com>
2017-10-11 13:07:37 +0000

    Set default modules

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,10 +1,7 @@
 # Add here the modules you want to enable here
 
-from modules.yesno import yesno
-from modules.wolfram import wolfram_mac
 from modules.hihelp import hihelp
 from modules.poll import poll
 from modules.poll2 import poll2
-from modules. elo import elo
 from modules import poker
 from modules import pokedex
