07e65963026e3d263ee86f3f38e2e84fde786253
iLQ <danielcardeenas@gmail.com>
2017-10-11 10:44:07 +0000

    Cleaning git

--- modules/elo/player.py
+++
@@ -1,6 +0,0 @@
-class Player(object):
-    def __init__(self, name, id_player, last_mu, rank):
-        self.name = name
-        self.rank = rank
-        self.id_player = id_player
-        self.last_mu = last_mu
