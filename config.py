c8fefe7a80481fbd3861109a647285fd3dc64759
iLQ <danielcardeenas@gmail.com>
2017-10-11 13:06:13 +0000

    Set default modules

---
+++ config.py
@@ -0,0 +1,11 @@
+credentials = dict(
+    phone = 'YOUR_PHONE',
+    password = 'YOUR_PASWWORD',
+)
+
+# Your contacts numbers (Framework will sync)
+# Add them here
+contacts = {
+    "CONTACT_PHONE": "CONTACT_NAME",
+    "ANOTHER_CONTACT_PHONE": "ANOTHER_CONTACT_NAME",
+}
