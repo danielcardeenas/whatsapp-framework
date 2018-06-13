2eb91d58d1fa60ed241a7e78e5f6f825b95179fd
Daniel Cardenas <danielcardeenas@gmail.com>
2018-06-13 09:58:11 -0500

    Update env_android.py

--- libs/yowsup/yowsup/env/env_android.py
+++ libs/yowsup/yowsup/env/env_android.py
@@ -17,9 +17,9 @@ class AndroidYowsupEnv(YowsupEnv):
         "YHNtYoIvt5R3X6YZylbPftF/8ayWTALBgcqhkjOOAQDBQADLwAwLAIUAKYCp0d6z4QQdyN74JDfQ2WCyi8CFDUM4CaNB+ceVXd" \
         "KtOrNTQcc0e+t"
 
-    _MD5_CLASSES = "EGazn6GwW4v5yFNGeI7xng=="
+    _MD5_CLASSES = "gmugC/SDpbuwyy3aHqR+IQ=="
     _KEY = "eQV5aq/Cg63Gsq1sshN9T3gh+UUp0wIw0xgHYT1bnCjEqOJQKCRrWxdAe2yvsDeCJL+Y4G3PRD2HUF7oUgiGo8vGlNJOaux26k+A2F3hj8A="
-    _VERSION = "2.18.176"
+    _VERSION = "2.18.181"
     _OS_NAME = "Android"
     _OS_VERSION = "4.3"
     _DEVICE_NAME = "endeavoru"
